b2cac7eb9270cf4708fb35c14a6e606c95574822
crmarques <crmarquesjc@gmail.com>
2020-08-02 13:27:39 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # simple-http-file-server
+
 A simple HTTP file server written in JavaScript.
+
+Server behavior can be customized by the following environment variables:
+
+| Variable | Default | Description |
+| PORT | 3000 | Port that file server will be listening. |
+| ACCESS_TOKEN | Randon generated UUID v4 token | Token used to authenticate to server. |
+| FILE_STORE_DIR | /tmp | Directory where files will be stored. |
+| FILE_SIZE_LIMITE | 10485760 (10Mb) | Max file size in bytes. |
---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const helmet = require('helmet')
+const path = require('path')
+const multer = require('multer');
+const uuid = require('uuid')
+
+const port = process.env.PORT || 3000
+const token = process.env.ACCESS_TOKEN || uuid.v4()
+const store = process.env.FILE_STORE_DIR || "/tmp"
+const fileLimit = process.env.FILE_SIZE_LIMITE || 10485760 // 10M
+
+if (!process.env.ACCESS_TOKEN)
+  console.log("Self-generated access token: " + token)
+
+const normalizedStore = path.normalize(store)
+
+const app = express()
+
+// security middlewares
+app.use(helmet())
+app.use((req, res, next) => {
+  if (req.headers.authorization === "Bearer " + token)
+    next()
+  else
+    res.sendStatus(403)
+})
+
+app.get('/', (req, res) => {
+  res.send('Welcome to simple http file server!')
+})
+
+app.use('/files', express.static(normalizedStore))
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, normalizedStore);
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  }),
+  limits: {
+    fileSize: fileLimit
+  },
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file)
+    res.json({
+      status: "error",
+      message: "Error saving file."
+    })
+  else
+    res.json({
+      status: "ok"
+    })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
