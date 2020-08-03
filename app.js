e9c3f5bb3debcfcd81181e96bee369aa6bcfa9fc
crmarques <crmarquesjc@gmail.com>
2020-08-03 18:00:48 -0300

    Fix initialization log

--- app.js
+++ app.js
@@ -58,5 +58,5 @@ app.post('/upload', upload.single('file'), (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Server listening at http://localhost:${port}`)
 })
